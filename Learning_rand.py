c254e0f4fe3d4dc0daa4ead29e5b1376af269006
Markus <markus2740@gmail.com>
2017-10-05 18:14:22 +0200

    saving most important files

---
+++ Learning_rand.py
@@ -0,0 +1,16 @@
+import Gann_test
+import time
+import tflowtools as TFT
+import tutor3
+
+#f_and_l = TFT.gen_all_parity_cases(3)
+#f = []
+#l = []
+#for x in f_and_l:
+#    f.append(x[0])
+#    l.append(x[1])
+#print( len(TFT.gen_all_parity_cases(3)))
+
+#TFT.dendrogram(f,l)
+
+gann = Gann_test.mapping_test()
